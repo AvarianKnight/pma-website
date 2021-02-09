991dac03bad9092360b2e969ab994c6682c71d09
AvarianKnight <dillskaggs@gmail.com>
2021-02-09 13:23:30 -0600

    add more information

--- src/components/Home.js
+++ src/components/Home.js
@@ -32,19 +32,25 @@ const Home = () => {
 	return (
 		<div className="home">
 			{!featured? failed? failed : 'Awating Data...' : ''}
-			{featured && featured.length > 0? <StreamerList streamers={featured} title="Featured Streamers" /> :
+			{featured && featured.length > 0 ? <StreamerList streamers={featured} title="Featured Streamers" /> : featured &&
 				(<div className="no-streamers">
 					<h1>Featured Streamers</h1>
 					<p>Theres currently no featured streamers online</p>
 				</div>)
 			}
-			{streamers && featured.length > 0 ? 
-				<StreamerList streamers={streamers} title="Streamers" /> :
+			{streamers && streamers.length > 0 ? 
+				<StreamerList streamers={streamers} title="Streamers" /> : streamers &&
 				(<div className="no-streamers">
 					<h1>Streamers</h1>
 					<p>Theres currently no streamers online</p>
 				</div>)
 			}
+
+			<div className="streamer-info">
+				<h1>How do I get put on here??</h1>
+				<p>In order to get put as regular streamer you will need to open a ticket on the <a href="http://discord.gg/pmarp">discord</a> and discuss it with a Community Manager+.</p>
+				<p>In order to get put as a featured streamer you have to display characteristics that we think will benefit the server, like wanting to extend interactions, making interactions for <strong>everyone</strong> and not just yourself, and overall be a good roleplayer.</p>
+			</div>
 		</div>
 	 );
 }
